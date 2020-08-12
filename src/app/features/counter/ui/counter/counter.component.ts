1e607a1565269dbf618a6c7609029925d0721ccc
Wataru.Kasahara <kasahara1019@gmail.com>
2020-08-12 21:49:26 +0900

    yarn ng g c features/counter/ui/counter

---
+++ src/app/features/counter/ui/counter/counter.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
