e3e488b8d80aee9d5a5f5bc207120fba37c29edc
Wataru.Kasahara <kasahara1019@gmail.com>
2020-08-13 21:37:03 +0900

    feat: Link to pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlankComponent } from './features/blank/ui/blank/blank.component';
 import { CounterComponent } from './features/counter/ui/counter/counter.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/counter', pathMatch: 'full' },
   { path: 'counter', component: CounterComponent },
+  { path: 'blank', component: BlankComponent },
 ];
 
 @NgModule({
