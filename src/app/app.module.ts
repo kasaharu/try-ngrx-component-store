8a4a9a8d072564cceadb258328b778a533e745f2
Wataru.Kasahara <kasahara1019@gmail.com>
2020-08-13 21:33:21 +0900

    yarn ng g c features/blank/ui/blank

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlankComponent } from './features/blank/ui/blank/blank.component';
 import { CounterComponent } from './features/counter/ui/counter/counter.component';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent],
+  declarations: [AppComponent, CounterComponent, BlankComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/features/blank/ui/blank/blank.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blank',
+  templateUrl: './blank.component.html',
+  styleUrls: ['./blank.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BlankComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
