ec7a5be31943d57ef1e27df22367386a77e8138d
Wataru.Kasahara <kasahara1019@gmail.com>
2020-10-10 17:14:55 +0900

    yarn ng g c features/counter/ui/counter-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterPageComponent } from './features/counter/ui/counter-page/counter-page.component';
 import { CounterComponent } from './features/counter/ui/counter/counter.component';
 import { HomeComponent } from './features/home/ui/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent, HomeComponent],
+  declarations: [AppComponent, CounterComponent, HomeComponent, CounterPageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/features/counter/ui/counter-page/counter-page.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-page',
+  templateUrl: './counter-page.component.html',
+  styleUrls: ['./counter-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CounterPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
