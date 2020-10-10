8bf050b0afa92ceef7094d7abc86d66a80a32052
Wataru.Kasahara <kasahara1019@gmail.com>
2020-10-10 17:07:28 +0900

    fix: BlankComponent の削除

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BlankComponent } from './features/blank/ui/blank/blank.component';
 import { CounterComponent } from './features/counter/ui/counter/counter.component';
 import { HomeComponent } from './features/home/ui/home/home.component';
 
@@ -8,7 +7,6 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'counter', component: CounterComponent },
-  { path: 'blank', component: BlankComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BlankComponent } from './features/blank/ui/blank/blank.component';
 import { CounterComponent } from './features/counter/ui/counter/counter.component';
 import { HomeComponent } from './features/home/ui/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent, BlankComponent, HomeComponent],
+  declarations: [AppComponent, CounterComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
