7d569467e794a95e4a9e18ec84f7f30a8fd1318f
Wataru.Kasahara <kasahara1019@gmail.com>
2020-10-10 17:00:23 +0900

    yarn ng g c features/home/ui/home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlankComponent } from './features/blank/ui/blank/blank.component';
 import { CounterComponent } from './features/counter/ui/counter/counter.component';
+import { HomeComponent } from './features/home/ui/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent, BlankComponent],
+  declarations: [AppComponent, CounterComponent, BlankComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
