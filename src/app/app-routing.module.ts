ada9edee587c6fa41bccf78a085b7dd997c30ab5
Wataru.Kasahara <kasahara1019@gmail.com>
2020-10-10 17:16:30 +0900

    feat: sub page に heading 表示

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CounterComponent } from './features/counter/ui/counter/counter.component';
+import { CounterPageComponent } from './features/counter/ui/counter-page/counter-page.component';
 import { HomeComponent } from './features/home/ui/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'counter', component: CounterComponent },
+  { path: 'counter', component: CounterPageComponent },
 ];
 
 @NgModule({
