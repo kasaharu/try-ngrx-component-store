55d975e3bb38c58320447df68800e5653f9142f8
Wataru.Kasahara <kasahara1019@gmail.com>
2020-10-10 17:02:40 +0900

    feat: ルーティングの整理

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlankComponent } from './features/blank/ui/blank/blank.component';
 import { CounterComponent } from './features/counter/ui/counter/counter.component';
+import { HomeComponent } from './features/home/ui/home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/counter', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'counter', component: CounterComponent },
   { path: 'blank', component: BlankComponent },
 ];
