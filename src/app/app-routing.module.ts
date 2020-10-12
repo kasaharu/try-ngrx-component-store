2548b5a7e368d29e9fa380aa199db14a5efa00be
Wataru.Kasahara <kasahara1019@gmail.com>
2020-10-12 19:50:17 +0900

    feat: Hero へのルーティング追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CounterPageComponent } from './features/counter/ui/counter-page/counter-page.component';
+import { HeroComponent } from './features/hero/ui/hero/hero.component';
 import { HomeComponent } from './features/home/ui/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'counter', component: CounterPageComponent },
+  { path: 'hero', component: HeroComponent },
 ];
 
 @NgModule({
